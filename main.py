c7c70f9ac55d803033d198d64fb7de963bfa7d30
Valerii <amabatman.12@gmail.com>
2024-10-14 19:50:15 +0600

    Added all classes

--- main.py
+++ main.py
@@ -4,6 +4,27 @@ class SavingAccount():
 class CheckingAccount():
     pass
 
+class Stock():
+    pass
+
+class Bond():
+    pass
+
+class Security(Stock, Bond):
+    pass
+
 class BankAccount(SavingAccount, CheckingAccount):
+    pass
+
+class RealEstate():
+    pass
+
+class InterestBearingItem(BankAccount, Security):
+    pass
+
+class Assest(BankAccount, RealEstate, Security):
+    pass
+
+class InsuableItem(BankAccount, RealEstate):
     pass
 
